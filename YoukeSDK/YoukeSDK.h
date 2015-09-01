ac740729708fe3b6f04f8d12465e8b945e673b45
duduluk101 <87215279@qq.com>
2015-09-02 07:32:40 +0800

    add new feature
    * 2015.8.31 增加：在聊天详情显示正在浏览的订单功能；
    * 2015.8.31 增加：在聊天详情显示的订单可以点击进入订单详情的接口；
    * 2015.9.1   增加：获取聊天列表的数据的接口；
    * 2015.9.2 增加：聊天详情页中的商品或订单被点击的事件；

--- YoukeSDK/YoukeSDK.h
+++ YoukeSDK/YoukeSDK.h
@@ -9,6 +9,25 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+
+/**
+ *  @author jxd, 15-09-01 10:09:20
+ *
+ *  聊天列表数据实体类
+ */
+@interface YoukeMessageListObject : NSObject
+
+@property (strong, nonatomic) NSString *fromUid;
+@property (strong, nonatomic) NSString *toUid;
+@property (strong, nonatomic) NSString *userName;
+@property (strong, nonatomic) NSString *userAvatar;
+@property (strong, nonatomic) NSString *userPhone;
+@property (strong, nonatomic) NSString *messageContent;
+@property (strong, nonatomic) NSString *messageTime;
+
+@end
+
+
 @interface YoukeSDK : NSObject
 
 /**
@@ -22,6 +41,31 @@
  */
 typedef void(^NEWMESSAGE_BLOCK)(NSInteger newMessageCount, NSString *messageContent, NSString *messageFrom);
 @property (nonatomic, copy) NEWMESSAGE_BLOCK  newMessageBlocker;
+
+
+/**
+ *  @author jxd, 15-09-01 17:09:01
+ *
+ *  聊天详情页的商品被点击，将通过这个bolock把商品id传出来
+ *
+ *  @param goodsId        被点击的商品的商品id
+ *  @param viewController 聊天详情页的实例变量，可以用该viewController来push或present商品详情页
+ */
+typedef void(^GOODSCLICK_BLOCK)(NSString *goodsId, UIViewController *viewController);
+@property (nonatomic, copy) GOODSCLICK_BLOCK goodsClickBlocker;
+
+
+/**
+ *  @author jxd, 15-09-01 17:09:08
+ *
+ *  聊天详情页中的订单被点击，将通过这个block把订单id传出来
+ *
+ *  @param orderId          订单id
+ *  @param viewController   聊天详情页的实例变量，可以用该viewController来push或present订单详情页
+ */
+typedef void(^ORDERCLICK_BLOCK)(NSString *orderId, UIViewController *viewController);
+@property (nonatomic, copy) ORDERCLICK_BLOCK orderClickBlocker;
+
 
 
 /**
@@ -55,7 +99,7 @@ typedef void(^NEWMESSAGE_BLOCK)(NSInteger newMessageCount, NSString *messageCont
 /**
  *  @author jxd, 15-05-25 17:05:13
  *
- *  提交商品及用户信息
+ *  提交商品及用户信息，访问商品页面时调用
  *
  *  @param goodsId      商品id(必传)
  *  @param goodsTitle   商品标题(必传)
@@ -78,6 +122,32 @@ typedef void(^NEWMESSAGE_BLOCK)(NSInteger newMessageCount, NSString *messageCont
                        Email:(NSString *)email
                           QQ:(NSString *)qq
                         Text:(NSString *)text;
+
+
+/**
+ *  @author jxd, 15-05-25 17:05:03
+ *
+ *  离开商品页面时调用
+ */
++ (void)leaveGoods;
+
+
+/**
+ *  @author jxd, 15-08-28 11:08:23
+ *
+ *  如果要在聊天列表显示正在浏览的订单，就要在打开聊天窗口之前调用该方法
+ *
+ *  @param orderId      订单号
+ *  @param orderTime    订单时间
+ *  @param goodsTitle   商品民称
+ *  @param goodsImage   商品图片
+ *  @param goodsPrice   商品价格
+ */
++ (void)visitOrderWithOrderId:(NSString*)orderId
+                    OrderTime:(NSString*)orderTime
+                   GoodsTitle:(NSString*)goodsTitle
+                   GoodsImage:(NSString*)goodsImage
+                   GoodsPrice:(NSString*)goodsPrice;
 
 
 /**
@@ -120,6 +190,20 @@ typedef void(^NEWMESSAGE_BLOCK)(NSInteger newMessageCount, NSString *messageCont
 
 
 /**
+ *  @author jxd, 15-08-30 12:08:58
+ *
+ *  获取点对点聊天列表数据，用于开发者需要自定义聊天列表界面的需求
+ *
+ *  @param userid  用户自己的用户id
+ *  @param success 成功block，成功的block会返回聊天列表的数组，数组元素为YoukeMessageListObject类型
+ *
+ */
++ (void)getPointToPointTalkListDataWithMyUserid:(NSString*)userid
+                                        success:(void (^)(NSArray *listArray)) success
+                                        failure:(void (^)(NSError *error)) failure;
+
+
+/**
  *  @author jxd, 15-06-17 06:06:37
  *
  *  打开帮助列表页面
@@ -149,13 +233,15 @@ typedef void(^NEWMESSAGE_BLOCK)(NSInteger newMessageCount, NSString *messageCont
  */
 - (id)getCurrentCommodity;
 
-
 /**
- *  @author jxd, 15-05-25 17:05:03
+ *  @author jxd, 15-08-28 11:08:44
  *
- *  离开商品页面时调用
+ *  获取正在浏览的订单
+ *
+ *  @return ZPFOrderObject
  */
-+ (void)leaveGoods;
+- (id)getCurrentOrder;
+
 
 
 @end
